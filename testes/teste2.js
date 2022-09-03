b5030c019cafb20f4ad11ade0206fb52698d6a8b
cesarfgs <cesarfgs@gmail.com>
2022-09-02 23:29:55 -0300

    aula

--- testes/teste2.js
+++ testes/teste2.js
@@ -1,10 +1,19 @@
-var nome = 'joão';
-var nota1 = 7;
-var nota2 = 9;
-var media = (nota1 + nota2) / 2;
+const somarNotas = function (nota1 ,nota2) {
+	console.log(nota1 + nota2);
+};
 
-console.log(media);
-console.log((nota1 + nota2) / 2);
-console.log('A aluno ' + nome + ' obteve média ' + media);
-console.log('A aluno ' + nome + ' obteve média ' + (nota1 + nota2) / 2);
-console.log('O aluno '.concat(nome).concat(' obteve média ').concat((nota1 + nota2) / 2));
+somarNotas(5.8 ,8.9);
+
+const somarNotas2 = (nota1 ,nota2) => {
+	console.log(nota1 + nota2);
+};
+
+somarNotas2(5.8 ,8.9);
+
+const somarNotas3 = (nota1 ,nota2) => console.log(nota1 + nota2);
+
+somarNotas3(5.8 ,8.9);
+
+const somarNotas4 = (nota1 ,nota2) => nota1 + nota2;
+
+console.log(somarNotas4(5.8 ,8.9));
