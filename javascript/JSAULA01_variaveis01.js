0658f477dadbd92e9fb28f8cf4fc4e62350d08b5
cesar <cesarfgs@gmail.com>
2022-09-05 09:58:24 -0300

    novas aulas javascript

---
+++ javascript/JSAULA01_variaveis01.js
@@ -0,0 +1,31 @@
+const idade = 18;
+console.log(idade);
+
+const IDADE = 18;
+console.log(IDADE);
+
+//const idade = 18;
+//var idade = 18;
+//let idade = 18;
+
+var informacao = 287;
+console.log(informacao);
+
+var informacao = 308;
+console.log(informacao);
+
+var informacao = '308';
+console.log(informacao);
+
+var informacao = 'INCCAA';
+console.log(informacao);
+
+var informacao = 'curso';
+console.log(informacao);
+
+var informacao = true;
+console.log(informacao);
+
+var informacao = Math.PI;
+console.log(informacao);
+
