4d96ec1c39bc4b99e52a27ded4d0363e6bcf90bc
César Felipe Gonçalves Silva <cesarfgs@jfal.jus.br>
2022-09-06 13:38:04 -0300

    escopo2

--- javascript/JSAULA02_escopos02.js
+++ javascript/JSAULA02_escopos02.js
@@ -1,1 +1,36 @@
-teste
+
+const declarandoComconst = 'const nivel 0';
+var declarandoComvar = 'var nivel 0';
+let declarandoComlet = 'let nivel 0';
+
+{
+    //console.log(declarandoComconst);
+    console.log(declarandoComvar);
+    //console.log(declarandoComlet);
+    
+    const declarandoComconst = 'const nivel 1';
+    var declarandoComvar = 'var nivel 1';
+    let declarandoComlet = 'let nivel 1';
+    {        
+        //console.log(declarandoComconst);
+        console.log(declarandoComvar);
+        //console.log(declarandoComlet);
+                
+        const declarandoComconst = 'const nivel 2';
+        var declarandoComvar = 'var nivel 2';
+        let declarandoComlet = 'let nivel 2';
+        {
+            //console.log(declarandoComconst);
+            console.log(declarandoComvar);
+            //console.log(declarandoComlet);
+
+            const declarandoComconst = 'const nivel 3';
+            var declarandoComvar = 'var nivel 3';
+            let declarandoComlet = 'let nivel 3';
+        }
+    }
+}
+
+console.log(declarandoComconst);
+console.log(declarandoComvar);
+console.log(declarandoComlet);
